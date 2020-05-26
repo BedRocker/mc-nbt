3d90ba768ae90e644aebd10a7852bb4c9d4caace
Alex Scott <alex@codetheoristdevelopments.co.uk>
2020-05-26 22:38:18 +0100

    0.1.0

--- src/compound.ts
+++ src/compound.ts
@@ -1,54 +1,67 @@
 module.exports = {
-  compound: [readCompound, writeCompound, sizeOfCompound]
-}
+  compound: [readCompound, writeCompound, sizeOfCompound],
+};
 
-function readCompound (buffer: any, offset: any, typeArgs: any, rootNode: any) {
+function readCompound(buffer: any, offset: any, typeArgs: any, rootNode: any) {
   const results: any = {
     value: {},
-    size: 0
-  }
+    size: 0,
+  };
   while (true) {
-    const typ = this.read(buffer, offset, 'i8', rootNode)
+    const typ = this.read(buffer, offset, 'i8', rootNode);
     if (typ.value === 0) {
-      offset += typ.size
-      results.size += typ.size
-      break
+      offset += typ.size;
+      results.size += typ.size;
+      break;
     }
 
-    const readResults = this.read(buffer, offset, 'nbt', rootNode)
-    offset += readResults.size
-    results.size += readResults.size
+    const readResults = this.read(buffer, offset, 'nbt', rootNode);
+    offset += readResults.size;
+    results.size += readResults.size;
     results.value[readResults.value.name] = {
       type: readResults.value.type,
-      value: readResults.value.value
-    }
+      value: readResults.value.value,
+    };
   }
-  return results
+  return results;
 }
 
-function writeCompound (value: any, buffer: any, offset: any, typeArgs: any, rootNode: any) {
-  const self = this
+function writeCompound(value: any, buffer: any, offset: any, typeArgs: any, rootNode: any) {
+  const self = this;
   Object.keys(value).map((key: any) => {
-    offset = self.write({
-      name: key,
-      type: value[key].type,
-      value: value[key].value
-    }, buffer, offset, 'nbt', rootNode)
-  })
-  offset = this.write(0, buffer, offset, 'i8', rootNode)
+    offset = self.write(
+      {
+        name: key,
+        type: value[key].type,
+        value: value[key].value,
+      },
+      buffer,
+      offset,
+      'nbt',
+      rootNode,
+    );
+  });
+  offset = this.write(0, buffer, offset, 'i8', rootNode);
 
-  return offset
+  return offset;
 }
 
-function sizeOfCompound (value: any, typeArgs: any, rootNode: any) {
-  const self = this
+function sizeOfCompound(value: any, typeArgs: any, rootNode: any) {
+  const self = this;
   // tslint:disable:no-shadowed-variable
   const size = Object.keys(value).reduce((size, key) => {
-    return size + self.sizeOf({
-      name: key,
-      type: value[key].type,
-      value: value[key].value
-    }, 'nbt', rootNode)
-  }, 0)
-  return 1 + size
+    return (
+      size +
+      self.sizeOf(
+        {
+          name: key,
+          type: value[key].type,
+          value: value[key].value,
+        },
+        'nbt',
+        rootNode,
+      )
+    );
+  }, 0);
+  return 1 + size;
 }
