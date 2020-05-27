a07bb18296000f626731693b8967f965825a2d5b
Alex Scott <alex@codetheoristdevelopments.co.uk>
2020-05-27 10:06:21 +0100

    0.1.6

--- src/data/sample.ts
+++ src/data/sample.ts
@@ -1,17 +1,17 @@
 // tslint:disable:no-var-requires
-const fs = require('fs')
-const nbt = require('../src/index')
+const fs = require('fs');
+const nbt = require('../src/index');
 
 fs.readFile('bigtest.nbt.gz', (error: any, data: any) => {
   if (error) {
-    throw error
+    throw error;
   }
 
   nbt.parse(data, (err: any, result: any) => {
     // tslint:disable:no-console
-    console.log(err)
-    console.log(result.value.stringTest)
-    console.log(result.value['nested compound test'])
-    console.log(nbt.simplify(result)['nested compound test'])
-  })
-})
+    console.log(err);
+    console.log(result.value.stringTest);
+    console.log(result.value['nested compound test']);
+    console.log(nbt.simplify(result)['nested compound test']);
+  });
+});
