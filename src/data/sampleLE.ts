16b167938a5c2766f29f4b75c2cca6b674866412
Alex Scott <alex@codetheoristdevelopments.co.uk>
2020-05-27 19:37:01 +0100

    0.1.13

--- src/data/sampleLE.ts
+++ src/data/sampleLE.ts
@@ -1,16 +1,16 @@
 export {};
 // tslint:disable:no-var-requires
-const fs = require('fs')
-const nbt = require('../../index')
+const fs = require('fs');
+const nbt = require('../../index');
 
 fs.readFile('level.dat', (error: any, data: any) => {
   if (error) {
-    throw error
+    throw error;
   }
 
   nbt.parse(data, true, (err: any, result: any) => {
     // tslint:disable:no-console
-    console.log(err)
-    console.log(JSON.stringify(result, null, 2))
-  })
-})
+    console.log(err);
+    console.log(JSON.stringify(result, null, 2));
+  });
+});
